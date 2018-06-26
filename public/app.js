a562c918b65e9487ca114f4e1d13c42957f2b512
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-06-25 18:43:52 -0600

    scrape is working

---
+++ public/app.js
@@ -0,0 +1,89 @@
+$("#scrape").on("click", function() {
+  // $.get("/scrape", function(data) {
+
+    // $("articles").empty();
+    // function scraperesult () {
+      $.getJSON("/articles", data => {
+        for (var i = 0; i < data.length; i++) {
+          let articleCont = "<div class='article-container' data-id=" + data[i]._id + ">"
+          let articlediv = "<div class='articlediv' data-id=" + data[i]._id + ">";
+          let newTitle = "<h4 id='artTitle'>" + data[i].title + "</h2>";
+          let newArtLink = "<a href='" + data[i].link + "'target='blank'>Click Here for story link</a>";
+          let noteButton = "<button id='notesbtn' data-id=" + data[i]._id + " class='btn btn-secondary'>Click For Notes</button>";
+
+          let articleTotal = (articleCont + articlediv + newTitle + "\n" + newArtLink + "\n" + noteButton)
+
+          $("#articles").append(articleTotal);
+        }
+      })
+    // }
+    // scraperesult ();
+  })
+// });
+
+
+
+
+$(document).on("click", "#notesbtn", function() {
+  $("#notes").empty();
+  var thisId = $(this).attr("data-id");
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    .then(function(data) {
+      // console.log(data);
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<button data-id='" + data.note._id + "' id='deletenote'>Delete Note</button>");
+
+      if (data.note) {
+        $("#titleinput").val(data.note.title);
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+$(document).on("click", "#savenote", function() {
+  var thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  })
+    
+    .then(function(data) {
+      
+      console.log(data);
+      
+      $("#notes").empty();
+    });
+
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+$(document).on("click", "#deletenote", function () {
+  let parentSelect = $(this).parent();
+  let selected = $(this).attr("data-id");
+
+  $.ajax({
+    type: "DELETE",
+    url: "/delete/" + selected,
+    
+    success: function(response) {
+    
+      $("#titleinput").val("");
+      $("#bodyinput").val("");
+      $("#action-button").html("<button id='make-new'>Submit</button>");
+    }
+    
+  });
+
+})
---
+++ server.js
@@ -0,0 +1,96 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+
+var PORT = 3000;
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/Mongo");
+
+// Routes
+
+app.get("/scrape", function(req, res) {
+  console.log("scrape");
+  
+  axios.get("https://www.wired.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    // console.log(response.data);
+    
+    $(".card-component").each(function(i, element) {
+      console.log("___________________");
+      
+      var result = {};
+      result.title = $(this)
+        .find("h2")
+        .text();
+        
+      result.link = 'https://www.wired.com' + $(this)
+        .find("a")
+        .attr("href");
+        // console.log(result.link);
+
+      console.log("Result" + JSON.stringify(result));
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // console.log(err);
+          
+          // return res.json(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  db.Article.find({}).then(dbArticle => {
+    res.json(dbArticle)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+});
+
+app.get("/articles/:id", function(req, res) {
+  let articleId = req.params.id;
+  db.article.findById(articleId)
+    .populate("note")
+    .then(article => res.json(article)
+    .catch(err => res.json(err))
+  )
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({_id: req.params.id}, { $push: {note: dbNote._id} }, {new: true});
+    })
+    .then(function(dbArticleNote) {
+      res.json(dbArticleNote);
+
+    })
+    .catch(function(err) {
+      res.json(err);
+    })
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
