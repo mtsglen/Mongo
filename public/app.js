4f986bd103f66b85df49126ad0be041ed085ce71
Stephanie Glendenning <mtfishergrl@hotmail.com>
2018-06-27 17:34:09 -0600

    final

--- public/app.js
+++ public/app.js
@@ -36,8 +36,8 @@ $(document).on("click", "#notesbtn", function() {
       $("#notes").append("<h2>" + data.title + "</h2>");
       // $("#notes").append("<input id='titleinput' name='title' >");
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-      $("#notes").append("<button data-id='" + data.note._id + "' id='deletenote'>Delete Note</button>");
+      $("#notes").append("<br><button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append("<br><button data-id='" + data.note._id + "' id='deletenote'>Delete Note</button>");
 
       if (data.note) {
         $("#titleinput").val(data.note.title);
